884c4cf3e832ffec27b0d47354d9c9069d7a8bf3
Arnab Majumder <arnabmajumdar399@gmail.com>
2024-07-26 02:27:58 +0530

    added map

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
-const express = require('express')
-const app = express()
-const http = require('http')
-const path = require('path')
+const express = require('express');
+const app = express();
+const http = require('http');
+const path = require('path');
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-const socketio = require('socket.io')
-const server = http.createServer(app)
+const socketio = require('socket.io');
+const server = http.createServer(app);
 
-const io = socketio(server)
+const io = socketio(server);
 
-app.set('view engine' , 'ejs')
-app.set(express.static(path.join(__dirname , 'public')))
+// Corrected the socket.io event name to "connection"
+io.on("connection", function(socket){
+    console.log("Connected");
+});
+
+// Set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// Serve static files from the 'public' directory using app.use
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
-    res.send('hello')
-})
+    res.render('index');
+});
 
-server.listen(PORT)
+// Added a callback to log when the server starts
+server.listen(PORT, function() {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
